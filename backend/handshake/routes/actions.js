5b8cb649d7ca8dcc3c4f7860e0e9078f9f1c9183
Zack Thompson <zst9886@rit.edu>
2015-10-04 03:32:44 -0400

    actions

---
+++ backend/handshake/routes/actions.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/getline', function(req, res, next){
+	switch(Math.floor((Math.random()*19)+1){
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+
+			break;
+		case 7:
+
+			break;
+		case 8:
+
+			break;
+		case 9:
+
+			break;
+		case 10:
+
+			break;
+		case 11:
+
+			break;
+		case 12:
+
+			break;
+		case 13:
+
+			break;
+		case 14:
+
+			break;
+		case 15:
+
+			break;
+		case 16:
+
+			break;
+		case 17:
+
+			break;
+		case 18:
+
+			break;
+		case 19:
+
+			break;
+	}
+});
+
+
