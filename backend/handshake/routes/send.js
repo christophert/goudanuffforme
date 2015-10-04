37ac8b450e4aedc062ba11c6fc7fed309547fad1
officer <me@christran.in>
2015-10-04 07:15:30 -0400

    ensure that second res isn't hit.

--- backend/handshake/routes/send.js
+++ backend/handshake/routes/send.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var configuration = require('../config.json');
+
+function getAPickupLine() {
+    return 'this should be a pickup line';
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +13,24 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     var phoneNum = req.body.phoneNum;
+    var client = require('twilio')(configuration.twilio.ACCT_SID, configuration.twilio.AUTH_TOKEN);
+
+    var pkline = getAPickupLine();
+
+    client.messages.create({
+        body: pkline,
+        to: "+1"+phoneNum,
+        from: "+15859783364"
+    }, function(err, message) {
+        res.send({
+            'status': 'ERROR',
+            'errmsg': message
+        });
+        return -1;
+    });
     res.send({
-        'pickUpLine':   req.body.phoneNum
+        'status': 'OK',
+        'pickUpLine': pkline
     });
 });
 
