1d6c7a3a303caae07e19f1426a73a64f6ac58ebf
Zack Thompson <zst9886@rit.edu>
2015-10-04 07:13:55 -0400

    actions or something

--- backend/handshake/routes/actions.js
+++ backend/handshake/routes/actions.js
@@ -7,9 +7,11 @@ router.get('/getline', function(req, res, next){
 	var word3;
 	var word4;
 	var result;
-	switch(Math.floor((Math.random()*17)+1){
+	switch(/*Math.floor((Math.random()*17)+1*/3){
 		case 1:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a4a67090f5a1551d064670f528901c73a351b7fedc7a3e898', function(error, response, body){
+				word4 = JSON.parse(body)['word'];
+			});
 			result = "Hey "+word1+" did you fall from "+word2+" because you're "+word3+" is "+word4;
 			break;
 		case 2:
@@ -25,7 +27,16 @@ router.get('/getline', function(req, res, next){
 			}
 			break;
 		case 3:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=verb&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5', functino(error, response, body){
+				word1 = JSON.parse(body)['word'];
+			});
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=noun-plural&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5', function(error, response, body){
+				word2 = JSON.parse(body)['word'];
+			});
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=noun-plural&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5', function(error, response, body){
+                word3 = JSON.parse(body)['word'];
+            });
+
 			result = "You make me "+word1+" like a "+word2+" on a(n) "+word3+".";
 			break;
 		case 4:
@@ -37,7 +48,9 @@ router.get('/getline', function(req, res, next){
 			result = "I must be a "+word1+" because I've fallen for you.";
 			break;
 		case 6:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a4a67090f5a1551d064670f528901c73a351b7fedc7a3e898', function(error, response, body){
+                word1 = JSON.parse(body)['word'];
+            });
 			result = "Hey, didn't I seee your name in the dictionary under "+word1+".";
 			break;
 		case 7:
@@ -54,7 +67,9 @@ router.get('/getline', function(req, res, next){
 				result = "Kiss me if I'm wrong, but tommorow's Friday, right?";
 			break;
 		case 9:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=noun&minCorpusCount=15000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5', function(error, response, body){
+				word1 = JSON.parse(body)['word'];
+			});
 			result = "If "+word1+" lasts forever, will you be my "+word1+"?";
 			break;
 		case 10:
@@ -62,7 +77,9 @@ router.get('/getline', function(req, res, next){
 			result = "Your "+word1+" look so lonely, would they like to meet mine?";
 			break;
 		case 11:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a4a67090f5a1551d064670f528901c73a351b7fedc7a3e898', functio(error, response, body){
+                word2 = JSON.parse(body)['word'];
+            });
 			result = "Hey, don't I know you? Yeah, you're the "+word1+" with the "+word2+" "+word3+".";
 			break;
 		case 12:
@@ -70,7 +87,9 @@ router.get('/getline', function(req, res, next){
 			result = "If I told you that you had a great "+word1+", would you hold it against me?";
 			break;
 		case 13:
-			//code
+			request('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a4a67090f5a1551d064670f528901c73a351b7fedc7a3e898', functio(error, response, body){
+                word1 = JSON.parse(body)['word'];
+            });
 			result = "Were you arrested earlier, because its gotta be illegal to look that "+word1+".";
 			break;
 		case 14:
