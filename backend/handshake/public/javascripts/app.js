87c3f690b224b2b043dc37000e6762982948ec37
officer <me@christran.in>
2015-10-04 06:12:42 -0400

    add frontend js and update layout

---
+++ backend/handshake/public/javascripts/app.js
@@ -0,0 +1,35 @@
+
+var getUrlParam = function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+$("#generateButtonEntry").submit(function(e) {
+    e.preventDefault();
+    
+    var serializedData = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url: "/api/send",
+        data: serializedData,
+        dataType: "json",
+        timeout: 2000,
+        cache: false,
+        success: function(r) {
+            $("#preform").hide();
+            $("#pickupLine").html(r.pickUpLine);
+            $("#pickupLine").show();
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
