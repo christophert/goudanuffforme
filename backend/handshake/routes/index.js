2c3026ad976e2b252b5b2e1000336699d39afaf9
officer <officer@enterprise.devices.christran.in>
2015-10-04 22:26:09 -0400

    push about

--- backend/handshake/routes/index.js
+++ backend/handshake/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Gouda \'nuff for me' });
 });
 
+router.get('/us', function(req, res, next) {
+    res.render('about', { title: 'About' });
+});
+
 module.exports = router;
